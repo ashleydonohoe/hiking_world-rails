4dbe7e7de0ee5be4936e81af01f610403f4285e3
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-12-19 23:55:20 +0000

    added picture model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218031621) do
+ActiveRecord::Schema.define(version: 20161219234743) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "image_url"
+    t.text    "caption"
+  end
 
   create_table "trails", force: :cascade do |t|
     t.string   "name"
